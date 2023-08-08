341f217008b4cc4558a758a2c09cfd4b949207fc
Nir Tamir <nirtamir2@gmail.com>
2023-08-08 19:29:13 +0300

    add buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World
+      <Button variant="default">Hello World</Button>
+      <Button variant="link">Hello World</Button>
+      <div>{JSON.stringify(buttonVariants)}</div>
     </main>
   );
 }

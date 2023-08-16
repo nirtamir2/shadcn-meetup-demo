d5f9c22af3e7bc19b7b6d664f11e1d9f42469c19
Nir Tamir <nirtamir2@gmail.com>
2023-08-16 10:49:40 +0300

    use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello World!
+        <div>
+
+      <Button variant="default">Hi there</Button>
+      <Button variant="ghost">Hi there</Button>
+        </div>
     </main>
   );
 }

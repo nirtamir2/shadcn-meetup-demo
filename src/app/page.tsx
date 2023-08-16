f2edab2ece976027063176c54ae2340e132abfef
Nir Tamir <nirtamir2@gmail.com>
2023-08-16 23:01:43 +0300

    Revert "shadcn ui init"
    This reverts commit e1d6f3fe1b5c53b419b38ed37a23ebe4a5a9967a.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
-import { Button } from "@/components/ui/button";
-
-
-function MyButtons() {
-  return (
-    <div className="flex gap-4">
-      <Button variant="default">Hi</Button>
-      <Button variant="outline">World</Button>
-    </div>
-  );
-}
-
 export default function Home() {
-  return (
-    <div className="flex flex-col gap-4 p-4">
-      <MyButtons />
-    </div>
-  );
+  return <div className="flex flex-col gap-4 p-4">Hello World</div>;
 }

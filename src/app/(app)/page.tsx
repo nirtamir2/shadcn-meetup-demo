b2557a2209593c9ece22c05c47a1562ea8d8acfe
Nir Tamir <nirtamir2@gmail.com>
2023-08-16 04:12:32 +0300

    add button

---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="default">Hello World</Button>
+      <Button variant="link">Hello World</Button>
+    </div>
+  );
+}

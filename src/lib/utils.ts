d01240fb3cbf4db017d407eecdffed7a04e2e817
Nir Tamir <nirtamir2@gmail.com>
2023-08-16 23:01:43 +0300

    Revert "shadcn ui init"
    This reverts commit f27f08eb522c873c75eded63df0b4951c9f7d238.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+// eslint-disable-next-line import/no-unused-modules
+export function cn(...inputs: Array<ClassValue>) {
+  return twMerge(clsx(inputs));
 }
